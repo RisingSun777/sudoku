1c03edce590341c6236334da5d905d5c074bcf7f
Do Nhat Minh <mrordinaire@gmail.com>
2012-12-03 16:11:24 +0800

    first pass at cleaning

---
+++ code/source/board_read.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "sudoku.h"
+ 
+void push(boardptr board, int row, int col, int val) {
+    int v = 0;
+    SETBIT(val, v);
+    board[row - 1][col - 1] = v;
+}
+
+void input(boardptr board) {
+    int isover = 0;
+    int col, row, val;
+    
+    while(!isover) {
+        printf("Enter row position: ");
+        scanf("%d", &row);
+        printf("Enter column position: ");
+        scanf("%d", &col);
+        printf("Enter value at row %d column %d: ", row, col);
+        scanf("%d", &val);
+        
+        push(board, row, col, val);
+    
+        printf("Continue adding (y/n)?");
+        if(getchar() == 'y')
+            isover = 1;
+    }
+}
---
+++ code/source/board_solve.c
@@ -0,0 +1,5 @@
+#include "sudoku.h"
+
+void solveSudoku(boardptr b) {
+
+}
