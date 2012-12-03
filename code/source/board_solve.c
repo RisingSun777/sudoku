c589cf74cf5b63d60f2d9a615fcdb4c07c4d2f24
RisingSun777 <superduong528@gmail.com>
2012-12-03 21:22:50 +0700

    Undone solver

--- code/source/board_solve.c
+++ code/source/board_solve.c
@@ -1,5 +1,26 @@
 #include "sudoku.h"
+#include "bitmanip.h"
 
 int board_solve(boardptr b) {
+    int temp = 0, val = 1;
+    for(int i = 0; i < MAXSIZE; i++) {
+        for(int j = 0; j < MAXSIZE; j++) {
+            val = 1;
+            do {
+                if(TESTBIT(val, b[i][j]) == BIT(val)) {
+                    b[i][j] = temp;
+                    board_setcell(b, i, j, val);
+                    val = 1;
+                    break;
+                }
+                else
+                    val++;
+            } while(val < 10);
+        }
+    }
+    
+    //if(!board_issolved(b))
+        //back one cell and start loop again
+    
     return -1;
 }
--- code/source/sudoku.c
+++ code/source/sudoku.c
@@ -4,6 +4,7 @@
 int main(void) {
     boardptr b = board_new();
     board_read(b);
+    //board_solve(b);
     board_print(b);
     board_delete(b);
 }
