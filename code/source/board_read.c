168d177abc2872e23c1e96641e19e021e18134e1
Do Nhat Minh <mrordinaire@gmail.com>
2012-12-04 10:25:04 +0800

    simpler read

--- code/source/board_read.c
+++ code/source/board_read.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "sudoku.h"
 #include "helpers.h"
+
+void ignorespace() {
+    int c;
+    while(isspace(c = getchar()))
+        ;
+    ungetc(c, stdin);
+}
  
 int board_read(boardptr b) {
     if (!b)
         return -1;
 
-    int val = 0;
+    int val, c;
     for (int i = 0; i < MAXSIZE; ++i)
         for (int j = 0; j < MAXSIZE; ++j) {
-            scanf("%d", &val);
-            if (val <= MAXVAL && val >= 1)
+            ignorespace();
+            c = getchar();
+            if (c <= '9' && c >= '0') {
+                val = c - '0';
+                //if (val <= MAXVAL && val >= 1)
                 board_setcell(b, i, j, val);
+            } else if (c == '.') {
+                // official ignored char
+            }
         }
 
     return 0;
